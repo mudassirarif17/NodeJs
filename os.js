a305a9831622491487d5f7b83f3a402e222afcd6
Mudassir <mudassirinoxent@gmail.com>
2023-05-22 14:54:56 +0500

    understanding about os module in node js

---
+++ os.js
@@ -0,0 +1,8 @@
+const os = require('node:os');
+console.log(os.totalmem());
+console.log(os.freemem());
+console.log(os.homedir());
+console.log(os.version());
+console.log(os.tmpdir());
+console.log(os.hostname());
+console.log(os.type());

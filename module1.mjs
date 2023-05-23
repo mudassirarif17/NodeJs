39b86fcf45496fa204ebc61666f0e1dfc9d472fd
Mudassir <mudassirinoxent@gmail.com>
2023-05-23 12:02:25 +0500

    understanding about common js and es6 module

---
+++ module1.mjs
@@ -0,0 +1,11 @@
+// common js module
+// function simple(){
+//     console.log("This is simple");
+// }
+
+// module.exports = simple;
+
+// es6 module
+export default function simple(){
+    console.log("This is simple");
+}
---
+++ module2.mjs
@@ -0,0 +1,5 @@
+// const simple = require("./module1");
+import simple from "./module1.mjs"
+
+// ES6 module
+simple();
